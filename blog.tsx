b2fe71681084b7cca949edddb87165a19fcb92d6
tdharris <tylerdavidharris@gmail.com>
2024-08-08 15:01:13 -0600

    fix(theme): force dark theme by default

--- blog.tsx
+++ blog.tsx
@@ -104,7 +104,7 @@ function errorHandler(err: unknown) {
  */
 export default async function blog(settings?: BlogSettings) {
   html.use(UnoCSS(settings?.unocss)); // Load custom unocss module if provided
-  html.use(ColorScheme("auto"));
+  html.use(ColorScheme("dark"));
 
   const url = callsites()[1].getFileName()!;
   const blogState = await configureBlog(url, IS_DEV, settings);

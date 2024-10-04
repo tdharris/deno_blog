485625b69504ac1707b78999a372ccf7cf1c9ffa
tdharris <tylerdavidharris@gmail.com>
2024-10-03 18:35:04 -0600

    fix(deno): always append file:// prefix

--- blog.tsx
+++ blog.tsx
@@ -173,7 +173,7 @@ export async function configureBlog(
   let directory;
 
   try {
-    const blogPath = fromFileUrl(IS_DEV ? `file://${url}` : url);
+    const blogPath = fromFileUrl(`file://${url}`);
     directory = dirname(blogPath);
   } catch (e) {
     console.error(e);
